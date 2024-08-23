07dbc67fb1d9626e8a91173e05e3e17121e732d7
Martin Arroyo <marroyo7775@gmail.com>
2024-08-22 22:32:01 -0400

    Forgot to bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiutils",
-    version="0.0.4",
+    version="0.0.5",
     description="A set of utility functions for AI projects",
     author="Martin Arroyo",
     author_email="martinm.arroyo7@gmail.com",
