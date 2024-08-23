c83c584d7497be8915a219146dbb5c71e5a5bd03
Martin Arroyo <marroyo7775@gmail.com>
2024-08-22 21:14:14 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-utils",
+    version="0.0.1",
+    description="A set of utility functions for AI projects",
+    author="Martin Arroyo",
+    author_email="martinm.arroyo7@gmail.com",
+    url="https://github.com/martinmarroyo/ai-utils",
+    packages=find_packages(),
+    install_requires=[
+        "rank_bm25",
+        "pydantic",
+        "aiohttp",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
