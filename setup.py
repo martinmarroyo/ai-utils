21baae98f21a01e2c41129a9d57047eda1654164
Martin Arroyo <marroyo7775@gmail.com>
2024-08-24 14:05:35 -0400

    Updated requirements version to correct weird issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiutils",
-    version="0.0.2",
+    version="0.0.3",
     description="A set of utility functions for AI projects",
     author="Martin Arroyo",
     author_email="martinm.arroyo7@gmail.com",
@@ -10,9 +10,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         "rank_bm25",
-        "pydantic",
-        "aiohttp",
-        "requests"
+        "pydantic>=2.8.2",
+        "aiohttp>=3.10.5",
+        "requests>=2.32.3"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
